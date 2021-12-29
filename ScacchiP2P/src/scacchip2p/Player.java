5f47a8f6034fb33b8dd8a0f76c094fb36370e69c
giorgia-l <lini.giorgiale@gmail.com>
2021-12-29 14:50:40 +0100

    modifica classe Player

--- ScacchiP2P/src/scacchip2p/Player.java
+++ ScacchiP2P/src/scacchip2p/Player.java
@@ -9,18 +9,33 @@ package scacchip2p;
  *
  * @author Giorgia
  */
-public class Player {
+public class Player {/*salva su file, punti e so
+                     che ho giocatore 
+                     con quel nome*/
     String nome;
     int porta;
 
-    public Player(String nome, /*int IP,*/ int porta) {
+    public Player(String nome, int IP, int porta) {
         this.nome = nome;
-        //this.IP = IP;
         this.porta = porta;
     }
 
     public Player() {
+    }        
+
+    public String getNome() {
+        return nome;
     }
-        
-    
+
+    public int getPorta() {
+        return porta;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPorta(int porta) {
+        this.porta = porta;
+    }    
 }
