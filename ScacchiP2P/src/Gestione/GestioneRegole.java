9c8c44261f7ba120df61f7733019be6265a1795f
giorgia-l <lini.giorgiale@gmail.com>
2022-01-03 15:32:29 +0100

    modifiche dovute all'aggiornamento del protocollo

--- ScacchiP2P/src/Gestione/GestioneRegole.java
+++ ScacchiP2P/src/Gestione/GestioneRegole.java
@@ -14,7 +14,6 @@ import scacchip2p.DatiCondivisi;
 public class GestioneRegole {
     
     DatiCondivisi dati=new DatiCondivisi();
-    int count = 0; //contatore che parte da 0 e quando arriva a 5 si chiude la connessione
     
     
     public GestioneRegole(DatiCondivisi dati) {
@@ -22,12 +21,12 @@ public class GestioneRegole {
     }
     
     public String amichevole(){
-        String messaggio = "r;" + count + 0;
+        String messaggio = "r;" + 0;
         return messaggio;
     }
     
     public String competitiva(){
-        String messaggio = "r;" + count + 1;
+        String messaggio = "r;" + 1;
         return messaggio;      
     }
     
@@ -35,7 +34,7 @@ public class GestioneRegole {
         String tempo = "";
         String aiuti = "";
         String tipoScacchi = "";
-        String messaggio = "r;" + count + 2 + tempo + aiuti + tipoScacchi;
+        String messaggio = "r;" + 2 + tempo + aiuti + tipoScacchi;
         return messaggio;
     } 
     
