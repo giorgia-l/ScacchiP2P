15b596e97ec746bb612f4c87d1b0c31c33ed8f75
giorgia-l <lini.giorgiale@gmail.com>
2022-01-04 12:30:19 +0100

    Aggiornamento classe "Regina"

--- ScacchiP2P/src/Pezzi/Regina.java
+++ ScacchiP2P/src/Pezzi/Regina.java
@@ -5,10 +5,40 @@
  */
 package Pezzi;
 
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import scacchip2p.Board;
+import scacchip2p.Moves;
+
 /**
  *
  * @author Giorgia
  */
-public class Regina {
-    
+public class Regina extends Pezzo {
+
+    public Regina(boolean white) {
+        super(white); //richiama la classe Pezzo 
+        if (isWhite() == true) {
+            ImageIcon ic = new ImageIcon("src/Pezzi.assets/regina-b.png");
+            piece = ic.getImage();
+        } else {
+            ImageIcon ic = new ImageIcon("src/Pezzi.assets/regina-n.png");
+            piece = ic.getImage();
+        }
+    }
+
+    @Override
+    public boolean canMove(Board board, Punto start, Punto end) {
+        if (end.getPiece().isWhite() == this.isWhite()) { //controllo che non ci sia un pezzo dello stesso colore nella cella 
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public ArrayList<Moves> getMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
