dc3563c4c94860f6b1609eec9eb7dc815d7e47f5
giorgia-l <lini.giorgiale@gmail.com>
2022-01-04 12:30:02 +0100

    Aggiornamento classe "Torre"

--- ScacchiP2P/src/Pezzi/Torre.java
+++ ScacchiP2P/src/Pezzi/Torre.java
@@ -5,10 +5,46 @@
  */
 package Pezzi;
 
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import scacchip2p.Board;
+import scacchip2p.Moves;
+
 /**
  *
  * @author Giorgia
  */
-public class Torre {
-    
+public class Torre extends Pezzo {
+
+    boolean arroccoFatto;
+
+    public Torre() {
+        arroccoFatto = false;
+    }
+
+    public Torre(boolean white) {
+        super(white); //richiama la classe Pezzo 
+        if (isWhite() == true) {
+            ImageIcon ic = new ImageIcon("src/Pezzi.assets/torre-b.png");
+            piece = ic.getImage();
+        } else {
+            ImageIcon ic = new ImageIcon("src/Pezzi.assets/torre-n.png");
+            piece = ic.getImage();
+        }
+    }
+
+    @Override
+    public boolean canMove(Board board, Punto start, Punto end) {
+        if (end.getPiece().isWhite() == this.isWhite()) { //controllo che non ci sia un pezzo dello stesso colore nella cella 
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public ArrayList<Moves> getMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
