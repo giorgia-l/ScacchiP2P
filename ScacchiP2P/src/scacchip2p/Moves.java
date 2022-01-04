a161cb1eecd9bb65f80f7d57323caf8aa5790374
giorgia-l <lini.giorgiale@gmail.com>
2022-01-04 12:25:28 +0100

    Creazione classe mosse

---
+++ ScacchiP2P/src/scacchip2p/Moves.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scacchip2p;
+
+import Pezzi.Pezzo;
+import Pezzi.Punto;
+
+/**
+ *
+ * @author Giorgia
+ */
+public class Moves {
+    Board board;
+    Pezzo piece;
+    Punto spot;
+}
