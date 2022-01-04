c7367fdc02155e5f35d3e9847c75549656ee65ad
salbeoo <albertostagnolavoro@gmail.com>
2022-01-04 12:03:34 +0100

    aggiunto messaggio partita personalizzata

--- ScacchiP2P/src/Gestione/GestioneRegole.java
+++ ScacchiP2P/src/Gestione/GestioneRegole.java
@@ -6,46 +6,51 @@
 package Gestione;
 
 import scacchip2p.DatiCondivisi;
+import scacchip2p.Peer;
 
 /**
  *
  * @author Giorgia
  */
 public class GestioneRegole {
-    
-    DatiCondivisi dati=new DatiCondivisi();
-    
-    
-    public GestioneRegole(DatiCondivisi dati) {
-        this.dati=dati;
+
+    Peer play1;
+
+    public GestioneRegole(Peer dati) {
+        this.play1 = play1;
     }
-    
-    public String amichevole(){
+
+    public String amichevole() {
         String messaggio = "r;" + 0;
         return messaggio;
     }
-    
-    public String competitiva(){
+
+    public String competitiva() {
         String messaggio = "r;" + 1;
-        return messaggio;      
+        return messaggio;
     }
-    
-    public String personalizzata(){
+
+    public String personalizzata() {
         String tempo = "";
         String aiuti = "";
         String tipoScacchi = "";
         String messaggio = "r;" + 2 + tempo + aiuti + tipoScacchi;
         return messaggio;
-    } 
-    
-    public String creoMessaggioRispostaY(){
-        String ris = "y;r";        
-        return ris;        
     }
-    
-    public String creoMessaggioRispostaN(){
-       String ris = "n;r;d";
-       return ris;
+
+    public String creoMessaggioRispostaY() {
+        String ris = "y;r";
+        return ris;
     }
-        
+
+    public String creoMessaggioRispostaN() {
+        String ris = "n;r;d";
+        return ris;
+    }
+
+    public String personalizzata(boolean tempo, boolean aiuti, String tipoScacchi) {
+        String messaggio = "r;" + 2 + ";" + tempo + ";" + aiuti + ";" + tipoScacchi;
+        return messaggio;
+    }
+
 }
