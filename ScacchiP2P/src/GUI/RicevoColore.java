d02e1403abb87cd8ea4e7535dd8c91125056a3ef
salbeoo <albertostagnolavoro@gmail.com>
2022-01-04 09:55:47 +0100

    modifica parte di invio risposta si o no

--- ScacchiP2P/src/GUI/RicevoColore.java
+++ ScacchiP2P/src/GUI/RicevoColore.java
@@ -40,6 +40,8 @@ public class RicevoColore extends javax.swing.JFrame {
     private void initComponents() {
 
         jTxtPlayer = new javax.swing.JLabel();
+        jTxtAvversarioColore = new javax.swing.JLabel();
+        jTxtPlayerColore = new javax.swing.JLabel();
         jTxtAvversario = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -48,25 +50,67 @@ public class RicevoColore extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jTxtPlayer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jTxtAvversario, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(24, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTxtAvversarioColore, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addComponent(jTxtPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTxtPlayerColore, javax.swing.GroupLayout.DEFAULT_SIZE, 148, Short.MAX_VALUE)))
+                .addGap(18, 18, 18))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(34, 34, 34)
+                    .addComponent(jTxtAvversario, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(162, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(19, 19, 19)
-                .addComponent(jTxtPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTxtPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTxtPlayerColore, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jTxtAvversario, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTxtAvversarioColore, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(25, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(72, Short.MAX_VALUE)
+                    .addComponent(jTxtAvversario, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(15, 15, 15)))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    public void ascolta() {
+        play1.getServer().start();
+
+        String messaggioRicevuto = play1.getServer().ascolta();
+        String campi[] = messaggioRicevuto.split(";");
+
+        if (campi[1].equals("bianco")) {
+//                    int risultato = JOptionPane.showConfirmDialog(null, "Accetti il " + campi[1] + "?", "Scelta colore?", JOptionPane.OK_CANCEL_OPTION);
+//
+//                    if (risultato == 1) {
+            play1.getGiocatore().setColore("nero");
+            play1.getDati().getPlayer2().setColore("bianco");
+//                    }
+        } else {
+            play1.getGiocatore().setColore("bianco");
+            play1.getDati().getPlayer2().setColore("nero");
+            ///decidere se chiudere, tornare alla paginaIniziale 
+        }
+    }
+
+    public void cambiaColore() {
+        jTxtPlayerColore.setText(play1.getGiocatore().getColore());
+        jTxtAvversarioColore.setText(play1.getDati().getPlayer2().getColore());
+    }
 
     /**
      * @param args the command line arguments
@@ -98,28 +142,8 @@ public class RicevoColore extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+
                 new RicevoColore(play1).setVisible(true);
-
-                String messaggioRicevuto = play1.getServer().ascolta();
-                String campi[] = messaggioRicevuto.split(";");
-
-                if (campi[1].equals("bianco")) {
-//                    int risultato = JOptionPane.showConfirmDialog(null, "Accetti il " + campi[1] + "?", "Scelta colore?", JOptionPane.OK_CANCEL_OPTION);
-//
-//                    if (risultato == 1) {
-                    play1.getGiocatore().setColore("nero");
-                    play1.getDati().getPlayer2().setColore("bianco");
-//                    }
-                } else {
-                    play1.getGiocatore().setColore("bianco");
-                    play1.getDati().getPlayer2().setColore("nero");
-                    ///decidere se chiudere, tornare alla paginaIniziale 
-                }
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(RicevoColore.class.getName()).log(Level.SEVERE, null, ex);
-                }
                 //far visualizzare la nuova pagina
             }
         });
@@ -127,6 +151,8 @@ public class RicevoColore extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jTxtAvversario;
+    private javax.swing.JLabel jTxtAvversarioColore;
     private javax.swing.JLabel jTxtPlayer;
+    private javax.swing.JLabel jTxtPlayerColore;
     // End of variables declaration//GEN-END:variables
 }
