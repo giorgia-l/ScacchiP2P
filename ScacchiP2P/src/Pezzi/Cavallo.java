49abfe608119cf5c97c13881d1d6681c4155b444
giorgia-l <lini.giorgiale@gmail.com>
2022-01-04 12:31:42 +0100

    Aggiornamento classe "Cavallo"

--- ScacchiP2P/src/Pezzi/Cavallo.java
+++ ScacchiP2P/src/Pezzi/Cavallo.java
@@ -5,10 +5,41 @@
  */
 package Pezzi;
 
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import scacchip2p.Board;
+import scacchip2p.Moves;
+
 /**
  *
  * @author Giorgia
  */
-public class Cavallo {
-    
+public class Cavallo extends Pezzo {
+
+    public Cavallo(boolean white) {
+        super(white); //richiama la classe Pezzo 
+        if (isWhite() == true){
+           ImageIcon ic = new ImageIcon("src/Pezzi.assets/cavallo-b.png");
+           piece = ic.getImage();
+        } else {
+           ImageIcon ic = new ImageIcon("src/Pezzi.assets/cavallo-n.png");
+           piece = ic.getImage();
+        }
+    }
+
+    @Override
+    public boolean canMove(Board board, Punto start, Punto end) {
+        if (end.getPiece().isWhite() == this.isWhite()) { //controllo che non ci sia un pezzo dello stesso colore nella cella 
+            return false;
+        }
+
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+        return x * y == 2;
+    }
+
+    @Override
+    public ArrayList<Moves> getMoves() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
