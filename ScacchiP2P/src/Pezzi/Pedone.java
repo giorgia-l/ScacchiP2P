cad4a263f2eabe7b635215b969f5dac8201a721e
giorgia-l <lini.giorgiale@gmail.com>
2022-01-07 18:23:18 +0100

    Conclusione(?) classe "Pedone", con i metodi per le mosse

--- ScacchiP2P/src/Pezzi/Pedone.java
+++ ScacchiP2P/src/Pezzi/Pedone.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import scacchip2p.Scacchiera;
 import scacchip2p.Moves;
+import static scacchip2p.Scacchiera.board;
 
 /**
  *
@@ -16,8 +17,8 @@ import scacchip2p.Moves;
  */
 public class Pedone extends Pezzo {
 
-    public Pedone(String name,boolean white) {
-        super(name,white); //richiama la classe Pezzo 
+    public Pedone(String name, boolean white) {
+        super(name, white); //richiama la classe Pezzo 
         if (isWhite() == true) {
             ImageIcon ic = new ImageIcon("src/Pezzi/assets/pedone-b.png");
             piece = ic.getImage();
@@ -28,17 +29,77 @@ public class Pedone extends Pezzo {
     }
 
     @Override
-    public boolean canMove(Scacchiera board, int xi, int xf, int yi, int yf) {
-//        if (end.getPiece().isWhite() == this.isWhite()) { //controllo che non ci sia un pezzo dello stesso colore nella cella 
-//            return false;
-//        }
-        
+    public boolean canMove(Punto[][] board, int xi, int xf, int yi, int yf) {
+
+        if (isWhite()) {
+            if (yi > yf) {
+                return false;
+            }
+        } else {
+            if (yf > yi) {
+                return false;
+            }
+        }
+
+        if (xi == xf) {
+            if (isWhite()) {
+                if (Scacchiera.board[xi][yi + 1] != null) {
+                    return false;
+                }
+            } else {
+                if (Scacchiera.board[xi][yi - 1] != null) {
+                    return false;
+                }
+            }
+
+            if (Math.abs(yf - yi) > 2) {
+                return false;
+            } else if (Math.abs(yf - yi) == 2) {
+                if (yi == 1 || yi == 6) { //I pedoni alla prima mossa possono procede anche di due
+                    return true;
+                }
+
+                if (isWhite()) {
+                    if (Scacchiera.board[xi][yi + 2] != null) {
+                        return false;
+                    }
+                } else {
+                    if (Scacchiera.board[xi][yi - 2] != null) {
+                        return false;
+                    }
+                }
+
+            }
+        }
         return true;
     }
 
     @Override
-    public ArrayList<Moves> getMoves() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public ArrayList<Moves> getMoves(int x, int y) {
+        ArrayList<Moves> m = new ArrayList<>();
+        ArrayList<Moves> mp = new ArrayList<>();
+        m.clear();
 
+        if (!this.isWhite()) {
+            if (y < 7) {
+                m.add(new Moves(x, y + 1));
+            }
+            if (y == 0) {
+                m.add(new Moves(x, y + 2));
+            }
+        } else {
+            if (y > 0) {
+                m.add(new Moves(x, y - 1));
+            }
+            if (y == 7) {
+                m.add(new Moves(x, y - 2));
+            }
+        }
+
+        for (Moves mossa : m) {
+            if (canMove(Scacchiera.board, x, y, mossa.x, mossa.y) == true);
+            mp.add(mossa);
+        }
+        return mp;
+    }
 }
