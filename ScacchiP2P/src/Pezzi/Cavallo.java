b6ee24aeb6943e3141a87c56fd035ea5cf8ed084
giorgia-l <lini.giorgiale@gmail.com>
2022-01-07 18:33:50 +0100

    Aggiornamenti dovuti alla modifiche della classe astratta "Pezzo"

--- ScacchiP2P/src/Pezzi/Cavallo.java
+++ ScacchiP2P/src/Pezzi/Cavallo.java
@@ -28,12 +28,12 @@ public class Cavallo extends Pezzo {
     }
 
     @Override
-    public boolean canMove(Scacchiera schr, int xi, int xf, int yi, int yf) {
+    public boolean canMove(Punto[][] board, int xi, int xf, int yi, int yf) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public ArrayList<Moves> getMoves() {
+    public ArrayList<Moves> getMoves(int x, int y) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
