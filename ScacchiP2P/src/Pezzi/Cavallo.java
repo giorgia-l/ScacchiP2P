9ccddec9e29a3f820c5ff5ad02ae659aee87f7d0
giorgia-l <lini.giorgiale@gmail.com>
2022-01-08 15:02:44 +0100

    Metodo per le mosse del cavallo

--- ScacchiP2P/src/Pezzi/Cavallo.java
+++ ScacchiP2P/src/Pezzi/Cavallo.java
@@ -31,7 +31,7 @@ public class Cavallo extends Pezzo {
     public boolean canMove(Punto[][] board, int xi, int xf, int yi, int yf) {
         /* il cavallo si muove a L e può "saltare" gli scacchi che si trovano nel percorso 
         prima di arrivare allla cella scelta, controlli: */
-        
+
         if (Math.abs(yf - yi) == 2 && Math.abs(xf - xi) == 1) {
             return true;
         }
@@ -45,16 +45,33 @@ public class Cavallo extends Pezzo {
 
     @Override
     public ArrayList<Moves> getMoves(int x, int y) {
-        
+
         ArrayList<Moves> m = new ArrayList<>();
         ArrayList<Moves> mp = new ArrayList<>();
         m.clear();
-        /*
-        controlli per aggiungere le mosse alla lista delle mosse possibili
-         */
+        mp.clear();
+
+        m.add(new Moves(x + 2, y + 1));
+        m.add(new Moves(x + 1, y + 2));
+        if (y - 1 >= 0) {
+            m.add(new Moves(x + 2, y - 1));
+            if (x - 2 >= 0) {
+                m.add(new Moves(x - 2, y - 1));
+                m.add(new Moves(x - 2, y + 1));
+            }
+        }
+        if (y - 2 >= 0) {
+            m.add(new Moves(x + 1, y - 2));
+            if (x - 1 >= 0) {
+                m.add(new Moves(x - 1, y - 2));
+            }
+        }
+        if (x - 1 >= 0) {
+            m.add(new Moves(x - 1, y + 2));
+        }
 
         for (Moves mossa : m) {
-            if (canMove(Scacchiera.board, x, y, mossa.x, mossa.y) == true);
+            if (canMove(Scacchiera.board, x, mossa.x, y, mossa.y) == true);
             mp.add(mossa);
         }
         return mp;
