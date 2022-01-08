4fb09d142a68a5ad52f2f36ac968bc600d2deffe
giorgia-l <lini.giorgiale@gmail.com>
2022-01-08 10:04:36 +0100

    Aggiornamento metodi per le mosse del Cavallo

--- ScacchiP2P/src/Pezzi/Cavallo.java
+++ ScacchiP2P/src/Pezzi/Cavallo.java
@@ -29,12 +29,35 @@ public class Cavallo extends Pezzo {
 
     @Override
     public boolean canMove(Punto[][] board, int xi, int xf, int yi, int yf) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /* il cavallo si muove a L e può "saltare" gli scacchi che si trovano nel percorso 
+        prima di arrivare allla cella scelta, controlli: */
+        
+        if (Math.abs(yf - yi) == 2 && Math.abs(xf - xi) == 1) {
+            return true;
+        }
+
+        if (Math.abs(yf - yi) == 1 && Math.abs(xf - xi) == 2) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
     public ArrayList<Moves> getMoves(int x, int y) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        ArrayList<Moves> m = new ArrayList<>();
+        ArrayList<Moves> mp = new ArrayList<>();
+        m.clear();
+        /*
+        controlli per aggiungere le mosse alla lista delle mosse possibili
+         */
+
+        for (Moves mossa : m) {
+            if (canMove(Scacchiera.board, x, y, mossa.x, mossa.y) == true);
+            mp.add(mossa);
+        }
+        return mp;
     }
 
 }
